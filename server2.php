7f96a17c412a57525255de80c21420550ce44ee9
Faiza <berrichif0717@gmail.com>
2021-06-15 07:45:22 +0200

    ajout else if

--- server2.php
+++ server2.php
@@ -5,8 +5,10 @@ $serv=strip_tags($_GET["serveur"]);
 $languages=strip_tags($_GET["langage"]);
   if(( $serv==$_SERVER["HTTP_HOST"])&& isset($_GET["langage"])){
     return" langage :".$languages."serveur: ".$serv;//d'abord on cree une fonction simple
+  }else if (empty($_GET["langage"])){
+    echo"erreur pas de langages séléctionné";
   }else{
-    echo"erreur";
+    echo "erreur";
   }
 }
   echo getError();
